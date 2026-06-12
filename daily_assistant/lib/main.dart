ab29ca9bd8894fd3767f7b82d9a2a5cfd3e62af3
AKSHAYARAJA <akshayarajae@gmail.com>
2026-06-12 16:32:08 +0000

    Test instant notification

--- daily_assistant/lib/main.dart
+++ daily_assistant/lib/main.dart
@@ -2,51 +2,41 @@ import 'package:flutter/material.dart';
 import 'services/notification_service.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await NotificationService.init();
+WidgetsFlutterBinding.ensureInitialized();
+await NotificationService.init();
 
-  runApp(const MyApp());
+runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+const MyApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Daily Assistant',
-      home: const HomePage(),
-    );
-  }
+@override
+Widget build(BuildContext context) {
+return MaterialApp(
+debugShowCheckedModeBanner: false,
+home: const HomePage(),
+);
+}
 }
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+const HomePage({super.key});
 
-  Future<void> scheduleTestNotification() async {
-    final time = DateTime.now().add(const Duration(seconds: 10));
-
-    await NotificationService.scheduleNotification(
-      id: 1,
-      title: 'Test Reminder',
-      body: 'Notification is working',
-      scheduledTime: time,
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Daily Assistant'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: scheduleTestNotification,
-          child: const Text('Test Notification'),
-        ),
-      ),
-    );
-  }
+@override
+Widget build(BuildContext context) {
+return Scaffold(
+appBar: AppBar(
+title: const Text('Daily Assistant'),
+),
+body: Center(
+child: ElevatedButton(
+onPressed: () async {
+await NotificationService.showInstantNotification();
+},
+child: const Text('Test Notification'),
+),
+),
+);
+}
 }
